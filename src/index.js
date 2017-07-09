d6c6c019ccfbd39ea3d0f0d0b65902dfc87e9664
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-09 16:35:34 -0700

    setup routes for products and cart

--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import { Router, Route, IndexRedirect, hashHistory } from 'react-router'
 
 import rootReducer from './reducers/rootReducer'
 
 import App from './components/App/App.jsx'
-
+import Products from './components/Products/Products.jsx'
+import Cart from './Components/Cart/Cart.jsx'
 const loggerMiddleware = createLogger()
 
 const store = createStore(
@@ -24,5 +26,11 @@ const store = createStore(
 
 render(
   <Provider store={store}>
-    <App />
+    <Router history={hashHistory}>
+      <Route path='/' component={App}>
+        <IndexRedirect to='/products'/>
+        <Route path='products' component={Products}/>
+        <Route path='cart' component={Cart} />
+      </Route>
+    </Router>
   </Provider>, document.getElementById('app'))
