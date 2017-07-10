091355508e482be0379112714e7f4bd7c1505598
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-09 23:33:59 -0700

    added better product IDs to api

--- server.js
+++ server.js
@@ -6,51 +6,49 @@ const app = express()
 app.get('/api/products', (req,res) => {
 
   return res.json({
-    products: [
-      {
-        id: 0,
+      50289449: {
+        id: 50289449,
         name: 'Jacket',
         description: 'An outdoor jacket to protect you from the elements',
         img: 'jacket.jpg',
         price: 25
       },
-      {
-        id: 1,
+      39944168: {
+        id: 39944168,
         name: 'Boots',
         description: 'Rugged boots, perfect for winter',
         img: 'boots.jpg',
         price: 35
       },
-      {
-        id: 2,
+      79871482: {
+        id: 79871482,
         name: 'Sweater',
         description: 'A nice and cozy sweater, perfect for winter',
         img: 'sweater.jpg',
         price: 20
       },
-      {
-        id: 3,
+      14211949: {
+        id: 14211949,
         name: 'Gloves',
         description: 'Warm gloves to keep your hands warm',
         img: 'gloves.jpg',
         price: 15
       },
-      {
-        id: 4,
+      68868330: {
+        id: 68868330,
         name: 'Googles',
         description: 'Protect your eyes from the sun\'s harsh rays',
         img: 'goggles.jpg',
         price: 20
       },
-      {
-        id: 5,
+      22732905: {
+        id: 22732905,
         name: 'Water Bottle',
         description: 'A bottle to keep you hydrated throughout the day',
         img: 'water-bottle.jpg',
         price: 5
       },
 
-    ]
   })
 })
 app.get('*', (req,res) => {
