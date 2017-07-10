54173175721f71f5dc63a16254df68dadbc911c2
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-09 22:36:40 -0700

    fixed cart spacing

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import CartItem from '../CartItem/CartItem.jsx'
 
 class Cart extends Component {
-  
+
   render() {
     return (
       <div className="Cart">
@@ -10,6 +10,7 @@ class Cart extends Component {
         <div className="cart-container">
           <div className="items">
             <h3>Items</h3>
+            <CartItem />
             <CartItem />
           </div>
           <div className="order-summary">
