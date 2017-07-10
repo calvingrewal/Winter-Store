6eab6df0c11460b35af88ac20873362b7ab328fd
Calvin Grewal <calvingrewal28@gmail.com>
2017-07-09 23:20:08 -0700

    added prices to api

--- server.js
+++ server.js
@@ -11,37 +11,43 @@ app.get('/api/products', (req,res) => {
         id: 0,
         name: 'Jacket',
         description: 'An outdoor jacket to protect you from the elements',
-        img: 'jacket.jpg'
+        img: 'jacket.jpg',
+        price: 25
       },
       {
         id: 1,
         name: 'Boots',
         description: 'Rugged boots, perfect for winter',
-        img: 'boots.jpg'
+        img: 'boots.jpg',
+        price: 35
       },
       {
         id: 2,
         name: 'Sweater',
         description: 'A nice and cozy sweater, perfect for winter',
-        img: 'sweater.jpg'
+        img: 'sweater.jpg',
+        price: 20
       },
       {
         id: 3,
         name: 'Gloves',
         description: 'Warm gloves to keep your hands warm',
-        img: 'gloves.jpg'
+        img: 'gloves.jpg',
+        price: 15
       },
       {
         id: 4,
         name: 'Googles',
         description: 'Protect your eyes from the sun\'s harsh rays',
-        img: 'goggles.jpg'
+        img: 'goggles.jpg',
+        price: 20
       },
       {
         id: 5,
         name: 'Water Bottle',
         description: 'A bottle to keep you hydrated throughout the day',
-        img: 'water-bottle.jpg'
+        img: 'water-bottle.jpg',
+        price: 5
       },
 
     ]
